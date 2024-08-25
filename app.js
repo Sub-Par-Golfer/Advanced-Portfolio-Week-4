0c4494cca900de72b0cd4eb1531cfe7015d603b9
Sub-Par-Golfer <joshjthompson@yahoo.com>
2024-08-25 17:40:02 -0400

    update

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll('.shape');
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += ' dark-theme'
+    }
+    else {
+        document.body.classList.remove('dark-theme')
+    }
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove('modal--open')
+    }
+    isModalOpen = !isModalOpen;
+    document.body.classList += ' modal--open'
+}
+
 // EmailJS codes
 //template_uheqp5t
 //service_yzkv3wp
@@ -24,12 +59,20 @@ function contact(event) {
         );
     })
 }
-let isModalOpen = false;
-function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove('modal--open')
-    }
-    isModalOpen = !isModalOpen;
-    document.body.classList += ' modal--open'
-}
+
+
+
+
+
+// function toggleModal() {
+//     if (isModalOpen) {
+//       isModalOpen = false;
+//       return document.body.classList.remove("modal--open");
+//     }
+//     isModalOpen = true;
+//     document.body.classList += " modal--open";
+//     window.scrollTo({
+//       top: 0,
+//       behavior: "smooth",
+//     });
+//   }
