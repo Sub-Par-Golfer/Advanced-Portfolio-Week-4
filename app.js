5b07829762b36233cca99c574266751c7d415235
Sub-Par-Golfer <joshjthompson@yahoo.com>
2024-08-24 23:51:26 -0400

    update

---
+++ app.js
@@ -0,0 +1,35 @@
+// EmailJS codes
+//template_uheqp5t
+//service_yzkv3wp
+//wciuI5vtvYyfew6j9
+// Code to send email from contact to my email below 
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += ' modal__overlay--visible'
+    emailjs
+    .sendForm(
+        'service_yzkv3wp',
+        'template_uheqp5t',
+        event.target,
+        'wciuI5vtvYyfew6j9'
+    ).then(() => {
+        loading.classList.remove('modal__overlay--visible')
+        success.classList += ' modal__overlay--visible'
+    }).catch(() => {
+        loading.classList.remove('modal__overlay--visible')
+        alert(
+            'The email service is temporarily unavailable. Please contact me at joshjthompson@yahoo.com'
+        );
+    })
+}
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove('modal--open')
+    }
+    isModalOpen = !isModalOpen;
+    document.body.classList += ' modal--open'
+}
